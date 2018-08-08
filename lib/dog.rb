653d86e1791639e17056cae469ff9a8a00b93c76
Yan Wang <Yan@Flatirons-MacBook-Air-4.local>
2018-08-08 17:56:00 -0400

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,23 @@
+class Dog
+
+  attr_accessor :name
+
+  @@all = []
+  @@all_names = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.clear_all
+    @@all.clear
+  end
+
+  def self.all
+    @@all.each do |dog|
+      puts dog.name
+    end
+  end
+
+end
